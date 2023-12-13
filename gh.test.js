0b216d53d2ed0fecb9c375051e443806fa0cdbb0
Gameunkulus <bugr24-8@mail.ru>
2023-12-13 16:45:55 +0300

    Homework 3

--- gh.test.js
+++ gh.test.js
@@ -4,8 +4,7 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
-  testTimeout: 60000
+  await page.goto("https://github.com/team")
 });
 
 afterEach(() => {
@@ -16,17 +15,14 @@ describe("Github page tests", () => {
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
-    await page.waitForSelector('h1', {
-      waitUntil: 'load',
-      setTimeout: 5000
-    });
+    await page.waitForSelector('h1', {setTimeout: 5000});
     const title2 = await page.title();
     expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
   });
 
   test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href'), {
-      waitUntil: 'load',
+    const actual = await page.$eval("a", link => link.getAttribute('href'), 
+    {
       setTimeout: 5000
     });
     expect(actual).toEqual("#start-of-content");
@@ -41,6 +37,34 @@ describe("Github page tests", () => {
       waitUntil: 'load',
       setTimeout: 5000
     });
-    expect(actual).toContain("Sign up for free")
+    expect(actual).toContain("Sign up for free");
+  });
+});
+
+
+describe("Homework 2", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/features/actions")
+  });
+
+  test("The h1 header other page content", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    const title2 = await page.title();
+    expect(title2).toEqual('Features • GitHub Actions · GitHub');
+  });
+
+  test("The first link other page attribute", async () => {
+    const actual = await page.$eval("a", link => link.getAttribute('href'));
+    expect(actual).toEqual("#start-of-content");
+  });
+
+  test("The other page contains Sign in button", async () => {
+    const btnSelector = ".btn-mktg.btn-large-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, link => link.textContent);
+    expect(actual).toContain("\n  Get started with Actions\n  \n  ");
   });
 });
